48d015b14a486fd0aaec0daf66b43a9c364a867e
PDC4-Training.PDC4 <PDC4-Training.PDC4@P4A-508SD52.dir.svc.accenture.com>
2018-08-02 10:47:05 +0530

    new commit

--- DevOps_1/src/test/java/skeleton/RunCukesTest.java
+++ DevOps_1/src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"})
+@CucumberOptions(plugin = {"pretty","json"},features="src/test/resources/skeleton")
 public class RunCukesTest {
 }
